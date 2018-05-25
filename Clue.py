0df0f6bb946740dc6eafe83fad0d4cd719a30e67
richardkarras <richard.karras@gmail.com>
2018-05-25 14:53:07 -0600

    loops built, need to add relay control

--- Clue.py
+++ Clue.py
@@ -11,18 +11,32 @@ counterMax = 30 #counter time in seconds before resetting registered input
 flagPerson = False #flag for correct person selected
 flagWeapon = False #flag for correct weapon selected
 #no flag for room since the rfid sensor will be behind the room on the game board
-
+#relay object - does it need to be a variable? 
 
 while const == 1
-    while counter < counterMax
+    while counter < counterMax:
         counter++
         tagIn = ser.readline()
         file = open("person.txt","r")
         fileIn = file.readline()
-        if tagIn == fileIn 
+        if tagIn == fileIn:
             flagPerson = True
+            counter = 0 #reset counter on valid input
         file.close()
         file = open("weapon.txt","r")
         fileIn = file.readline()
-        if tagIn == fileIn
-            flagWeapon = True 
+        if tagIn == fileIn:
+            flagWeapon = True
+            counter = 0 #reset counter on valid input
+        file.close()
+        if flagPerson == True:
+            if flagWeapon == True:
+                #trigger relay open
+            else:
+                #trigger relay closed
+        else:
+            #trigger relay closed
+        time.sleep (1) #sleep for 1 second between passes
+    #end of while counter < counterMax loop
+    flagPerson = False; flagWeapon = False; counter = 0
+    #loop to beginning of while counter < counterMax loop
